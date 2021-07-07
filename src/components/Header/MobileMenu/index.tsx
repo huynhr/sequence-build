dc914aed59f8001c1181c03be7a1e7b7109ac8fd
Ray <huynh.r.k@gmail.com>
2021-07-06 19:43:06 -0700

    Add mobile header

---
+++ src/components/Header/MobileMenu/index.tsx
@@ -0,0 +1,43 @@
+/** @jsxImportSource @emotion/react */
+import { useState } from 'react'
+import HamburgerMenu from '../../../assets/mobile_menu.png'
+import MobileLogo from '../../../assets/mobile_logo.png'
+import CloseIcon from '../../../assets/close_button.png'
+
+const MobileMenu = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <div css={{ width: '100%' }}>
+      <section css={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+        <button type="button" onClick={() => setMenuOpen(true)}>
+          <img src={HamburgerMenu} alt="menu" css={{ height: '55px' }} />
+        </button>
+      </section>
+      {menuOpen && (
+        <div
+          css={{
+            background:
+              'linear-gradient(83.57deg,transparent 13.55%,rgba(34,201,191,.05) 98.35%),linear-gradient(265.77deg,transparent 44.4%,rgba(92,47,115,.2) 110.31%),#111',
+            height: '509px',
+            left: '0px',
+            position: 'absolute',
+            top: '0px',
+            width: '100%',
+          }}
+        >
+          <section
+            css={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '24px' }}
+          >
+            <img src={MobileLogo} alt="logo" css={{ height: '55px' }} />
+            <button type="button" onClick={() => setMenuOpen(false)}>
+              <img src={CloseIcon} alt="close menu" css={{ height: '55px' }} />
+            </button>
+          </section>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default MobileMenu
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { useMediaQuery } from 'react-responsive'
 import Logo from '../Logo'
+import MobileMenu from './MobileMenu'
 
 const Header = () => {
-  const isDesktopOrLaptop = useMediaQuery({
+  const isDesktop = useMediaQuery({
     query: '(min-device-width: 768px)',
   })
 
@@ -17,6 +18,7 @@ const Header = () => {
         'paddingTop': '24px',
         'paddingLeft': '24px',
         'paddingRight': '24px',
+        'position': 'relative',
         'marginRight': 'auto',
         'marginLeft': 'auto',
         '@media(min-width: 640px)': {
@@ -24,7 +26,8 @@ const Header = () => {
         },
       }}
     >
-      {isDesktopOrLaptop && <Logo />}
+      {isDesktop && <Logo />}
+      {!isDesktop && <MobileMenu />}
     </div>
   )
 }
